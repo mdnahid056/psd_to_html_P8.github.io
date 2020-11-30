80c9f761ec9bddb886f28a2a5bd5b0950335412e
Ferdaos <54438631+whaiteking@users.noreply.github.com>
2020-11-30 09:12:19 -0800

    Psd_to_html_8
    this is main file  upload

---
+++ js/min.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+    $(".navbar-toggler, .overlay").on("click", function(){
+        $(".sideNav, .overlay").toggleClass("open");
+    })
+});
+
+
+function openNav() {
+  document.getElementById("sideNav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("sideNav").style.width = "0";
+}
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:3
+      }
+  }
+});
+$(function() {
+  var selectedClass = "";
+  $(".filter").click(function(){
+  selectedClass = $(this).attr("data-rel");
+  $("#gallery").fadeTo(100, 0.1);
+  $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+  setTimeout(function() {
+  $("."+selectedClass).fadeIn().addClass('animation');
+  $("#gallery").fadeTo(300, 1);
+  }, 300);
+  });
+  });
+
+    
+ //Get the button
+ var mybutton = document.getElementById("myBtn");
+
+ // When the user scrolls down 20px from the top of the document, show the button
+ window.onscroll = function() {scrollFunction()};
+ 
+ function scrollFunction() {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+     mybutton.style.display = "block";
+   } else {
+     mybutton.style.display = "none";
+   }
+ }
+ 
+ // When the user clicks on the button, scroll to the top of the document
+ function topFunction() {
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
+ }
